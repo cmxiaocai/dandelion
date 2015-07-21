b73c56b11c24eaa74a1dbfda63f3d54c061cd36f
cmxiaocai <cmxiaocai@163.com>
2015-07-21 17:26:32 +0800

    发布页面js代码提交

--- application/controllers/Project.php
+++ application/controllers/Project.php
@@ -3,18 +3,20 @@
 class ProjectController extends Yaf\Controller_Abstract{
 
     public function versionAction($project_name){
-        
         $ProjectEntity = new \services\project\Entity( $project_name );
         $VersionDepot  = new \services\versiondepot\Entity( $ProjectEntity );
         $version_list  = $VersionDepot->getBranchs();
-
         $this->getView()->assign('version_list', $version_list);
         $this->getView()->assign('version_path', $ProjectEntity->depo_path);
         $this->getView()->assign('project_name', $ProjectEntity->project_name);
     }
 
     public function publishAction($project_name){
-
+        $ProjectEntity = new \services\project\Entity( $project_name );
+        $VersionDepot  = new \services\versiondepot\Entity( $ProjectEntity );
+        $version_list  = $VersionDepot->getBranchs();
+        $this->getView()->assign('version_list', $version_list);
+        $this->getView()->assign('version_path', $ProjectEntity->depo_path);
         $this->getView()->assign('project_name', $project_name);
     }
 
